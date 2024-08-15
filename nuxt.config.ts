9e63245d97af780ce5578fe6baca57a9cb7d256b
Ornella <ornella@Ornellas-MacBook-Pro.local>
2024-08-15 12:16:02 +0200

    project init

---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import { resolve } from 'path';
+
+
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@primevue/nuxt-module',
+  ],
+  primevue: {
+    options: {
+      unstyled: true
+    },
+    importPT: { from: resolve(__dirname, './presets/aura/') }
+  }
+});
